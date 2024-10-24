82bdcd70da7b1adfb7f55a3718c5c7edf8438b5b
hyemiie <yemiojedapo1@gmail.com>
2024-10-24 14:04:06 +0100

    deployment commit2

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 10000, // or any other port Render expects
+    host: '0.0.0.0', // Listen on all interfaces
+  },
+
 })
